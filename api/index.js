3b4ec4a452a8279f48f27a717a61b17f2e4fdd2b
golam mostafa <gmnayeem25@gmail.com>
2023-01-12 01:14:56 +0600

    setup all basic process

---
+++ api/index.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+// package.json code
+// "start": "node index.js",
+//     "server": "nodemon index.js",
+//     "client": "npm start --prefix ../client",
+//     "admin": "npm start --prefix ../admin",
+//     "dev": "concurrently \"npm run server\" \"npm run client\" "
