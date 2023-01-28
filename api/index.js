5e98221e33b93729047d54833e4076a22d71f3b2
golam mostafa <gmnayeem25@gmail.com>
2023-01-29 01:31:53 +0600

    netflix backend done successfully

--- api/index.js
+++ api/index.js
@@ -1,9 +1,51 @@
+// external import
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+
+// internal import
+const authRoute = require('./routes/auth')
+const userRoute = require('./routes/user')
+const movieRoute = require('./routes/movie')
+const listRoute = require('./routes/list')
+
+const app = express();
 
 
+// middlewares
+app.use(morgan('dev'));
+app.use(cors());
+
+// body-parser
+app.use(express.json());
 
 
+// routes
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+app.use("/api/movies", movieRoute)
+app.use("/api/lists", listRoute)
 
 
+app.get("/", (req, res) => {
+    res.status(200).json("HI, I am from server!!")
+});
+
+
+// port and database connection
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server in running at http://localhost:${PORT}`);
+    mongoose.connect(
+        process.env.MONGO_URL,
+        { useNewUrlParser: true },
+        () => {
+            console.log('Database Connected Successfully...');
+        }
+    )
+});
 
 
 
